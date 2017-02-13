093090cbd2155d7742c241483573875d7609c773
HervePerdry <herve.perdry@gmail.com>
2017-02-13 11:11:52 +0100

    added test.snps test.inds which.snps which.inds

--- R/bm_select.r
+++ R/bm_select.r
@@ -20,7 +20,37 @@ select.inds <- function(x, condition) {
   x[w,]
 }
 
+test.snps <- function(x, condition, na.to.false = TRUE) {
+  if(!is(x, "bed.matrix")) stop("x is not a bed.matrix")
+  w <- eval(substitute(condition), x@snps, parent.frame())
+  miss <- is.na(w)
+  if(na.to.false & sum(miss)>0)
+    return(w & !miss)
+  w
+}
+
+test.inds <- function(x, condition, na.to.false = TRUE) {
+  if(!is(x, "bed.matrix")) stop("x is not a bed.matrix")
+  w <- eval(substitute(condition), x@ped, parent.frame())
+  miss <- is.na(w)
+  if(sum(miss)>0)
+    return(w & !miss)
+  w
+}
+
+which.snps <- function(x, condition) {
+  if(!is(x, "bed.matrix")) stop("x is not a bed.matrix")
+  which(eval(substitute(condition), x@snps, parent.frame()))
+}
+
+which.inds <- function(x, condition) {
+  if(!is(x, "bed.matrix")) stop("x is not a bed.matrix")
+  which(eval(substitute(condition), x@ped, parent.frame()))
+}
+
 is.autosome <- function(chr) chr %in% getOption("gaston.autosomes")
 is.chr.x    <- function(chr) chr %in% getOption("gaston.chr.x")
 is.chr.y    <- function(chr) chr %in% getOption("gaston.chr.y")
 is.chr.mt   <- function(chr) chr %in% getOption("gaston.chr.mt")
+
+
