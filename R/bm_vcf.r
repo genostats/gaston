970e2debb56a1cacd6ab6b9d06964bfc8b4952e0
HervePerdry <herve.perdry@gmail.com>
2017-02-16 14:59:37 +0100

    read vcf update / beginning read vcf filtered

--- R/bm_vcf.r
+++ R/bm_vcf.r
@@ -11,7 +11,16 @@ read.vcf <- function(file, max.snps, get.info = FALSE, convert.chr = TRUE, verbo
   } else {
     samples <- WhopGenome::vcf_getselectedsamples(file)
     which.samples <- WhopGenome::vcf_getsamples(file) %in% samples
-    f <- function() WhopGenome::vcf_readLineRawFiltered(file) 
+    # vcf_readLineRawFiltered doesn't deal well with filtered lines
+    # -> we need this kludge to get filtered raw lines with the same
+    #    behavior as vcf_readLineRaw
+    f <- function() {
+      a <- WhopGenome::vcf_readLineVecFiltered(file) 
+      if(is.null(a)) 
+        FALSE
+      else
+        paste(a, collapse = "\t")
+    }
   }
 
   if(missing(max.snps)) max.snps = -1L;
@@ -19,6 +28,60 @@ read.vcf <- function(file, max.snps, get.info = FALSE, convert.chr = TRUE, verbo
   if(verbose) cat("Reading diallelic variants for", length(samples), "individuals\n");
   
   L <- .Call("gg_read_vcf2", PACKAGE="gaston", f, which.samples, max.snps, get.info)
+  if(!is.null(xx)) WhopGenome::vcf_close(xx)
+
+  snp <- data.frame(chr = L$chr, id = L$id, dist = 0, pos = L$pos , A1 = L$A1, A2 = L$A2, 
+                    quality = L$quality, filter = factor(L$filter), stringsAsFactors = FALSE)
+  if(get.info) snp$info <-  L$info
+  if(convert.chr) {
+    chr <- as.integer(L$chr)
+    chr[L$chr == "X"  | L$chr == "x"]  <- getOption("gaston.chr.x")[1]
+    chr[L$chr == "Y"  | L$chr == "y"]  <- getOption("gaston.chr.y")[1]
+    chr[L$chr == "MT" | L$chr == "mt"] <- getOption("gaston.chr.mt")[1]
+    if(any(is.na(chr))) 
+      warning("Some unknown chromosomes id's (try to set convert.chr = FALSE)")
+    snp$chr <- chr
+  } 
+
+  ped <- data.frame(famid = samples, id = samples, father = 0, mother = 0, sex = 0, pheno = NA, stringsAsFactors = FALSE)
+  x <- new("bed.matrix", bed = L$bed, snps = snp, ped = ped,
+           p = NULL, mu = NULL, sigma = NULL, standardize_p = FALSE,
+           standardize_mu_sigma = FALSE )
+
+  if(getOption("gaston.auto.set.stats", TRUE)) x <- set.stats(x, verbose = verbose)
+  x
+}
+
+read.vcf.filtered <- function(file, positions, max.snps, get.info = FALSE, convert.chr = TRUE, verbose = getOption("gaston.verbose",TRUE)) {
+  xx <- NULL;
+  if(is.character(file)) {
+    filename <- path.expand(file)
+    xx <- WhopGenome::vcf_open(file)
+    if(is.null(xx)) stop("File not found")
+    samples <- WhopGenome::vcf_getsamples(xx) 
+    which.samples <- rep(TRUE, length(samples))
+    WhopGenome::vcf_selectsamples( xx, samples )
+    f <- function() WhopGenome::vcf_readLineRaw(xx) 
+  } else {
+    samples <- WhopGenome::vcf_getselectedsamples(file)
+    which.samples <- WhopGenome::vcf_getsamples(file) %in% samples
+    # vcf_readLineRawFiltered doesn't deal well with filtered lines
+    # -> we need this kludge to get filtered raw lines with the same
+    #    behavior as vcf_readLineRaw
+    f <- function() {
+      a <- WhopGenome::vcf_readLineVecFiltered(file) 
+      if(is.null(a)) 
+        FALSE
+      else
+        paste(a, collapse = "\t")
+    }
+  }
+
+  if(missing(max.snps)) max.snps = -1L;
+
+  if(verbose) cat("Reading diallelic variants for", length(samples), "individuals\n");
+  
+  L <- .Call("gg_read_vcf_filtered", PACKAGE="gaston", f, positions, which.samples, max.snps, get.info)
   if(!is.null(xx)) WhopGenome::vcf_close(xx)
 
   snp <- data.frame(chr = L$chr, id = L$id, dist = 0, pos = L$pos , A1 = L$A1, A2 = L$A2, 
--- src/m4_read_vcf_2.cpp
+++ src/m4_read_vcf_2.cpp
@@ -35,7 +35,6 @@ void set(uint8_t * data, size_t j, uint8_t val) {
 }
 
 
-// [[Rcpp::export]]
 List read_vcf2(Function f, LogicalVector samples, int max_snps, bool get_info) {
  
   List L;
@@ -157,6 +156,144 @@ List read_vcf2(Function f, LogicalVector samples, int max_snps, bool get_info) {
   return L;
 }
 
+
+// lecture VCF filtré par chr / pos
+// POS est une liste de positions dont les clefs sont les ids des chromosomes
+// on suppose que les positions sont triées
+// aucune supposition n'est faite sur l'ordre des variants dans le VCF
+inline bool is_ok(std::string chr, int pos, List POS) {
+  if(!POS.containsElementNamed(chr.c_str())) {
+    return false;
+  }
+  IntegerVector a( as<IntegerVector>(POS[chr.c_str()]) );
+  return std::binary_search(a.begin(), a.end(), pos);
+}
+
+// [[Rcpp::export]]
+List read_vcf_filtered(Function f, List POS, LogicalVector samples, int max_snps, bool get_info) {
+ 
+  List L;
+  std::vector<std::string> chr, id, ref, alt, filter, info;
+  std::vector<int> pos;
+  std::vector<double> qual;
+  int nsamples = sum(samples);
+ 
+  XPtr<matrix4> pX(new matrix4(0, nsamples));  // avec nrow = 0 allocations() n'est pas appelé
+
+  std::vector<uint8_t *> data;
+  uint8_t * data_;
+
+  int i = 0;
+  while(i != max_snps) {
+    int pos_ = 0;
+    std::string chr_, id_("(no SNP read yet)"), ref_, alt_, filter_, info_;
+    double qual_;
+
+    SEXP fres = f();
+    if (TYPEOF(fres) == LGLSXP && !as<bool>(fres)) { // eof
+      break;
+    }
+    if (TYPEOF(fres) != STRSXP) {
+      Rf_error("VCF error, unexpected result from read function");
+    }
+    CharacterVector x(fres);
+    char * a = (char *) x[0];
+    char * t = a;
+
+    if(str_token_tab(a,t)>0) chr_.assign(t);     // CHR
+    else Rf_error("VCF format error, last SNP read %s chr = %s pos %d", id_.c_str(), chr_.c_str(), pos_);
+
+    if(str_token_tab(a,t)>0) pos_ = atoi(t);     // POS
+    else Rf_error("VCF format error, last SNP read %s chr = %s pos %d", id_.c_str(), chr_.c_str(), pos_);
+
+    if(!is_ok(chr_, pos_, POS)) continue;        // filtrage
+
+    if(str_token_tab(a,t)>0) id_.assign(t);      // ID
+    else Rf_error("VCF format error, last SNP read %s chr = %s pos %d", id_.c_str(), chr_.c_str(), pos_);
+
+    if(str_token_tab(a,t)>0) ref_.assign(t);     // REF
+    else Rf_error("VCF format error while reading SNP %s chr = %s pos %d", id_.c_str(), chr_.c_str(), pos_);
+
+    if(str_token_tab(a,t)>0) alt_.assign(t);     // ALT
+    else Rf_error("VCF format error while reading SNP %s chr = %s pos %d", id_.c_str(), chr_.c_str(), pos_);
+
+    if(alt_.find(',') != std::string::npos) continue;  // on ne continue que s'il y a un seul allèle alternatif
+
+    if(str_token_tab(a,t)>0) qual_ = atof(t);    // QUAL
+    else Rf_error("VCF format error while reading SNP %s chr = %s pos %d", id_.c_str(), chr_.c_str(), pos_);
+
+    if(str_token_tab(a,t)>0) filter_.assign(t);  // FILTER
+    else Rf_error("VCF format error while reading SNP %s chr = %s pos %d", id_.c_str(), chr_.c_str(), pos_);
+
+    if(str_token_tab(a,t)>0) info_.assign(t);    // INFO
+    else Rf_error("VCF format error while reading SNP %s chr = %s pos %d", id_.c_str(), chr_.c_str(), pos_);
+
+
+    if(str_token_tab(a,t)>0) {                   // FORMAT
+      char * b;
+      if(str_token_col(t,b)>0) {
+        if(strcmp(b,"GT") != 0) continue;        // doit commencer par GT.
+      }
+      else Rf_error("VCF format error while reading SNP %s chr = %s pos %d", id_.c_str(), chr_.c_str(), pos_);
+    } else Rf_error("VCF format error while reading SNP %s chr = %s pos %d", id_.c_str(), chr_.c_str(), pos_);
+
+
+    // maintenant qu'on sait qu'on va garder cette ligne, on fait nos push back
+    chr.push_back(chr_);
+    pos.push_back(pos_);
+    id.push_back(id_);
+    ref.push_back(ref_);
+    alt.push_back(alt_);
+    qual.push_back(qual_); 
+    filter.push_back(filter_);
+    if(get_info) info.push_back(info_);
+    // nouvelle ligne de données    
+    data_ = new uint8_t [pX->true_ncol];
+    std::fill(data_, data_ + pX->true_ncol, 255); // c'est important de remplir avec 3 -> NA
+
+    int j = 0;
+    for(int j1 = 0; j1 < samples.length(); j1++) {
+      char * b;
+      int g = 0;
+      if(str_token_tab(a,t) == 0)
+        Rf_error("VCF format error while reading SNP read %s", id_.c_str());
+      if(!samples[j1]) continue;
+      int le = str_token_col(t,b);
+      if(le == 3) { // deux allèles 0/0 0/1 1/1 ou 0|0 etc  
+        if(*b == '1') g++;
+        if(*(b+2) == '1') g++;
+        if( (*b == '.') || (*(b+2) == '.')) g = 3;  // valeurs manquantes = .
+      } else if(le == 1) { // un allèle (cas haploide)
+        if(*b == '1') g = 1; else if(*b == '.') g = 3;
+      } else {
+        g = 3; // set to NA
+      }
+      set(data_, j, g);
+      j++;
+    }
+
+    data.push_back(data_);
+    i++;
+  }
+  
+  // et on finit la construction de la matrice
+  pX->nrow = i; 
+  pX->data = new uint8_t * [i];
+  for(int j = 0; j < i; j++) pX->data[j] = data[j];
+
+  L["id"] = id;
+  L["pos"] = pos;
+  L["chr"] = chr;
+  L["A1"] = ref;
+  L["A2"] = alt;
+  L["quality"] = qual;
+  L["filter"] = filter;
+  if(get_info) L["info"] = info;
+
+  L["bed"] = pX;
+  return L;
+}
+
 RcppExport SEXP gg_read_vcf2(SEXP fSEXP, SEXP samplesSEXP, SEXP maxsnpSEXP, SEXP giSEXP) {
 BEGIN_RCPP
     SEXP __sexp_result;
@@ -171,6 +308,20 @@ BEGIN_RCPP
     }
     UNPROTECT(1);
     return __sexp_result;
+END_RCPP
+}
+
+RcppExport SEXP gg_read_vcf_filtered(SEXP fSEXP, SEXP POSSEXP, SEXP samplesSEXP, SEXP max_snpsSEXP, SEXP get_infoSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< Function >::type f(fSEXP);
+    Rcpp::traits::input_parameter< List >::type POS(POSSEXP);
+    Rcpp::traits::input_parameter< LogicalVector >::type samples(samplesSEXP);
+    Rcpp::traits::input_parameter< int >::type max_snps(max_snpsSEXP);
+    Rcpp::traits::input_parameter< bool >::type get_info(get_infoSEXP);
+    rcpp_result_gen = Rcpp::wrap(read_vcf_filtered(f, POS, samples, max_snps, get_info));
+    return rcpp_result_gen;
 END_RCPP
 }
 
