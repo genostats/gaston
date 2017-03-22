7afab3650f889755b33463090e2aff8b36c43fca
HervePerdry <herve.perdry@gmail.com>
2017-03-22 22:01:28 +0100

    continued

--- src/diago_newton.h
+++ src/diago_newton.h
@@ -8,31 +8,31 @@ using namespace Eigen;
 
 // structure qui hérite fun pour l'optimisation par Brent
 // Normalement l'utilisation doit être
-// T_Matrix = MatrixXd, T_Vector = VectorXd, T_scal = double
-// T_Matrix = MatrixXf, T_Vector = VectorXf, T_scal = float
-template<typename T_Matrix, typename T_Vector, typename T_scal>
-class diag_likelihood : public fun {
+// MATRIX = MatrixXd, VECTOR = VectorXd, scalar_t = double
+// MATRIX = MatrixXf, VECTOR = VectorXf, scalar_t = float
+template<typename MATRIX, typename VECTOR, typename scalar_t>
+class diag_likelihood : public fun<scalar_t> {
   public:
     int p, n, r;
-    T_Matrix Y;
-    T_Matrix X;
-    T_Matrix Sigma;
-    T_Vector P0y;
-    T_scal & v;
-    T_Matrix XViX_i;
-    T_Vector Deltab;
-    T_scal likelihood;
-    T_scal d, log_d;
-    T_Vector V0b, V0bi;
-    T_Matrix ViX, XViX;
-    T_scal yP0y;
-    diag_likelihood(int p, const T_Matrix & Y, const T_Matrix & X, const T_Vector & Sigma, 
-                   T_Vector & P0y, T_scal & v, T_Matrix & XViX_i) : p(p), n(Sigma.rows()), r(X.cols()), Y(Y), X(X), Sigma(Sigma), P0y(P0y), v(v), XViX_i(XViX_i) {
-      Deltab = Sigma.bottomRows(n-p) - T_Vector::Ones(n-p);
+    MATRIX Y;
+    MATRIX X;
+    MATRIX Sigma;
+    VECTOR P0y;
+    scalar_t & v;
+    MATRIX XViX_i;
+    VECTOR Deltab;
+    scalar_t likelihood;
+    scalar_t d, log_d;
+    VECTOR V0b, V0bi;
+    MATRIX ViX, XViX;
+    scalar_t yP0y;
+    diag_likelihood(int p, const MATRIX & Y, const MATRIX & X, const VECTOR & Sigma, 
+                   VECTOR & P0y, scalar_t & v, MATRIX & XViX_i) : p(p), n(Sigma.rows()), r(X.cols()), Y(Y), X(X), Sigma(Sigma), P0y(P0y), v(v), XViX_i(XViX_i) {
+      Deltab = Sigma.bottomRows(n-p) - VECTOR::Ones(n-p);
     } ;
 
-    void update(T_scal h2) {
-      V0b = h2*Sigma.bottomRows(n-p) + (1-h2)*T_Vector::Ones(n-p); 
+    void update(scalar_t h2) {
+      V0b = h2*Sigma.bottomRows(n-p) + (1-h2)*VECTOR::Ones(n-p); 
       V0bi = V0b.cwiseInverse();
       ViX = V0bi.asDiagonal() * X.bottomRows(n-p);  //      V_{0b}^{-1} X_b
       XViX = X.bottomRows(n-p).transpose() * ViX;   // X_b' V_{0b}^{-1} X_b
@@ -43,47 +43,76 @@ class diag_likelihood : public fun {
       v = yP0y / (n-r-p);
     }
 
-    T_scal f(T_scal h2) {
+    scalar_t f(scalar_t h2) {
       update(h2);
-      likelihood = -0.5*(V0b.array().log().sum() + log_d + (n-r-p)*log(yP0y) + (n-r-p)*(1-log((T_scal)(n-r-p))) ); // avec le terme constant
+      likelihood = -0.5*(V0b.array().log().sum() + log_d + (n-r-p)*log(yP0y) + (n-r-p)*(1-log((scalar_t)(n-r-p))) ); // avec le terme constant
       return likelihood;
     }
 
-    void df_ddf(T_scal h2, T_scal & df, T_scal & ddf) {
+    void df_ddf(scalar_t h2, scalar_t & df, scalar_t & ddf) {
       update(h2);
-      T_Vector DeltaP0y = Deltab.cwiseProduct( P0y );
-      T_scal y_PDP_y = P0y.dot( DeltaP0y );
+      VECTOR DeltaP0y = Deltab.cwiseProduct( P0y );
+      scalar_t y_PDP_y = P0y.dot( DeltaP0y );
   
-      // T_Vector P0DeltaP0y = P0b * DeltaP0y;
-      T_Vector P0DeltaP0y  = V0bi.asDiagonal() * DeltaP0y - ViX * ( XViX_i * ( ViX.transpose() * DeltaP0y ) ) ;
-      T_scal y_PDPDP_y = DeltaP0y.dot( P0DeltaP0y );
+      // VECTOR P0DeltaP0y = P0b * DeltaP0y;
+      VECTOR P0DeltaP0y  = V0bi.asDiagonal() * DeltaP0y - ViX * ( XViX_i * ( ViX.transpose() * DeltaP0y ) ) ;
+      scalar_t y_PDPDP_y = DeltaP0y.dot( P0DeltaP0y );
 
-      // T_scal f = -0.5*(V0b.array().log().sum() + log_d + (n-r-p)*log(yP0y)+ (n-r-p)*(1-log((T_scal)(n-r-p))));
+      // scalar_t f = -0.5*(V0b.array().log().sum() + log_d + (n-r-p)*log(yP0y)+ (n-r-p)*(1-log((scalar_t)(n-r-p))));
       // **** calcul dérivées
-      // T_Matrix P0b = V0bi.asDiagonal();
+      // MATRIX P0b = V0bi.asDiagonal();
       // P0b.noalias() -= ViX * XViX_i * ViX.transpose();
-      // T_Matrix DeltaP0 = Deltab.asDiagonal() * P0b;
-      // T_scal df0 = DeltaP0.trace() - (n-r-p)*y_PDP_y/yP0y;
-      // T_scal ddf0 = -trace_of_product(DeltaP0, DeltaP0) + (n-r-p)*( 2*y_PDPDP_y/yP0y - (y_PDP_y*y_PDP_y)/(yP0y*yP0y));
+      // MATRIX DeltaP0 = Deltab.asDiagonal() * P0b;
+      // scalar_t df0 = DeltaP0.trace() - (n-r-p)*y_PDP_y/yP0y;
+      // scalar_t ddf0 = -trace_of_product(DeltaP0, DeltaP0) + (n-r-p)*( 2*y_PDPDP_y/yP0y - (y_PDP_y*y_PDP_y)/(yP0y*yP0y));
 
       // alternative qui évite le calcul de P0b et Delta P0b (qui sont rapides à calculer
       // mais de grande taille ->O(n²r) remplacé par O(r²n) si je ne m'abuse
-      T_Matrix B = ViX.transpose() * Deltab.asDiagonal() * ViX;
-      T_Matrix C = ViX.transpose() * (Deltab.cwiseProduct(V0bi.cwiseProduct(Deltab))).asDiagonal() * ViX;
-      T_Matrix AB = XViX_i * B;
-      T_scal trace_PD = Deltab.cwiseProduct(V0bi).sum() - AB.trace();
-      T_scal trace_PDPD = Deltab.cwiseProduct(V0bi.cwiseProduct(Deltab.cwiseProduct(V0bi))).sum() - 2*trace_of_product(XViX_i, C) + trace_of_product(AB,AB);
+      MATRIX B = ViX.transpose() * Deltab.asDiagonal() * ViX;
+      MATRIX C = ViX.transpose() * (Deltab.cwiseProduct(V0bi.cwiseProduct(Deltab))).asDiagonal() * ViX;
+      MATRIX AB = XViX_i * B;
+      scalar_t trace_PD = Deltab.cwiseProduct(V0bi).sum() - AB.trace();
+      scalar_t trace_PDPD = Deltab.cwiseProduct(V0bi.cwiseProduct(Deltab.cwiseProduct(V0bi))).sum() - 2*trace_of_product(XViX_i, C) + trace_of_product(AB,AB);
   
       df = trace_PD - (n-r-p)*y_PDP_y/yP0y;
       ddf = -trace_PDPD + (n-r-p)*( 2*y_PDPDP_y/yP0y - (y_PDP_y*y_PDP_y)/(yP0y*yP0y));
     }
-    void newton(T_scal & h2, const T_scal min_h2, const T_scal max_h2, const T_scal eps, const bool verbose);
+
+    void beta_blup(scalar_t h2, VECTOR & beta, VECTOR & omega) {
+      update(h2);
+      beta_blup(beta, omega);
+    }
+
+    // *********** CALCUL DES BLUPS ************************
+    // Attention P0y n'est que (P0y)b, les n-p dernières composantes ! (les p premières sont nulles)
+    void beta_blup(VECTOR & beta, VECTOR & omega) {
+      VECTOR Sigmab = sigma.bottomRows(n-p);
+      VECTOR omega = h2 * sigmab.asDiagonal() * P0y;
+
+      VECTOR z = y;
+      z.tail(n-p) -= omega + (1-h2)*P0y;
+      // Xb' Xb
+      MATRIX xtx( MATRIX(r,r).setZero().selfadjointView<Lower>().rankUpdate( x.bottomRows(n-p).transpose() ));
+      MATRIX xtx0( xtx );
+      MATRIX xtxi(r,r); // et son inverse
+      double d1, ld1;
+      sym_inverse(xtx0, xtxi, d1, ld1, 1e-5); // détruit xtx0
+
+      VECTOR beta(r+p);
+      beta.topRows(r) = xtxi * x.bottomRows(n-p).transpose() * z.bottomRows(n-p);
+      beta.bottomRows(p) = z.topRows(p) - x.topRows(p) * beta.topRows(r);
+    }
+
+    void newton(scalar_t & h2, const scalar_t min_h2, const scalar_t max_h2, const scalar_t eps, const bool verbose);
 };
 
-template<typename T_Matrix, typename T_Vector, typename T_scal>
-void diag_likelihood<T_Matrix, T_Vector, T_scal>::newton(T_scal & h2, const T_scal min_h2, const T_scal max_h2, const T_scal eps, const bool verbose) {
+
+
+
+template<typename MATRIX, typename VECTOR, typename scalar_t>
+void diag_likelihood<MATRIX, VECTOR, scalar_t>::newton(scalar_t & h2, const scalar_t min_h2, const scalar_t max_h2, const scalar_t eps, const bool verbose) {
   int nb_reseeds = 0, i = 0;
-  T_scal df = 1+2*eps; 
+  scalar_t df = 1+2*eps; 
 
   bool tried_max = false, tried_min = false;
   if(h2 == min_h2) tried_min = true;
@@ -91,7 +120,7 @@ void diag_likelihood<T_Matrix, T_Vector, T_scal>::newton(T_scal & h2, const T_sc
 
   while(std::abs(df) > 2*eps) {
 
-    T_scal ddf;
+    scalar_t ddf;
     df_ddf(h2, df, ddf);
 
     if(verbose) {
@@ -112,7 +141,7 @@ void diag_likelihood<T_Matrix, T_Vector, T_scal>::newton(T_scal & h2, const T_sc
     // si la convexité est mauvaise
     if(ddf < 0) {
       if(verbose) Rcout << "[Iteration " << i << "] likelihood isn't concave" << std::endl;
-      T_scal old_h2 = h2;
+      scalar_t old_h2 = h2;
       if(df < 0) {
         if(!tried_max) {
           h2 = max_h2; 
