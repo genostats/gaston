c8f80148d536ee0b39285a2875c006b9de364dce
HervePerdry <herve.perdry@gmail.com>
2017-05-25 08:42:40 +0200

    auto set #threads

--- R/zzz.r
+++ R/zzz.r
@@ -1,4 +1,9 @@
 .onLoad <- function(libname, pkgname) {
   options(gaston.chr.x = 23L, gaston.chr.y = 24L, gaston.chr.mt = 26L, gaston.autosomes = c(1:22,25L) )
-  rnorm(1); # force seed initialisation (not done when the RNG is called from C++ code)
+  rnorm(1); # force RNG seed initialisation (not done when the RNG is called from C++ code)
+
+  n <- RcppParallel::defaultNumThreads()
+  n <- 2**max(0,floor(min(3, log2(n) - 1)))
+  RcppParallel::setThreadOptions(n)
+  packageStartupMessage("Gaston set number of threads to ", n, ". Use RcppParallel::setThreadsOptions() to modify this.")
 }
